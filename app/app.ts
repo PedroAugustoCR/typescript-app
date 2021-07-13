7d5e86241208969f604cea1f98106a1d3f1d2a93
PedroAugustoCR <pedroaugusto_cr@hotmail.com>
2021-07-13 12:29:41 -0300

    Update and increment files

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,8 @@
-alert("oi");
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  controller.adiciona();
+})
